9e49b7dd763626049b115117c1e6121df98b88a1
raynedys <raynoldklubis@gmail.com>
2020-06-07 08:48:31 +0700

    sisa shop dan cart yang belum

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
         autoplayTimeout: 5000,
         loop:true,
         nav:true,
-        navText:["<img src='img/banner/prev.png'>","<img src='img/banner/next.png'>"],
+        navText:["PREV","NEXT"],
         dots:false
     });
 
@@ -67,7 +67,7 @@ $(document).ready(function(){
         autoplayTimeout: 5000,
         loop:true,
         nav:true,
-        navText:["<img src='img/product/prev.png'>","<img src='img/product/next.png'>"],
+        navText:["PREV","NEXT"],
         dots:false
     });
 
