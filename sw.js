047622e72c572b13f274bf791c2ffd09d5369da7
raynedys <raynoldklubis@gmail.com>
2020-06-25 22:44:03 +0700

    perubahan

--- sw.js
+++ sw.js
@@ -93,7 +93,7 @@ self.addEventListener('fetch', function(event) {
             .catch(function(err) {
               return caches.open(CACHE_STATIC_NAME)
                 .then(function(cache) {
-                  return cache.match('/offline.html');
+                  return cache.match('./offline.html');
                 });
             });
         }
